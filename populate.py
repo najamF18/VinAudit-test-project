fd53f17947004bf9146c7a7bd2f94c8d74aa45ce
najam <najamazam420@gmail.com>
2023-01-27 01:42:18 -0600

    updated populate.py to cater multiple db files

--- populate.py
+++ populate.py
@@ -8,30 +8,46 @@ django.setup()
 import time
 from avg_price_calculator.models import Car
 
+lines = []
+objs = []
+field_names = []
 
-with open("car_data.txt", "r") as f:
-    lines = [line.strip() for line in f]
+with open("data_reduced1.txt", "r") as f:
+    lines = lines + [line.strip() for line in f]
     field_names = lines[0].split("|")
     lines = lines[1:]
-    objs = []
-
-    program_starts = time.time()
-    for line in lines:
-        new_dict = dict()
-        for field, value in zip(field_names, line.split("|")):
-            if value == "":
-                print("null spotted")
-                value = None
-
-            if value == "TRUE":
-                value = True
-            if value == "FALSE":
-                value = False
-            new_dict[field] = value
-
-        objs.append(Car(**new_dict))
-        now = time.time()
-    Car.objects.bulk_create(objs)
-
 
 f.close()
+   
+
+with open("data_reduced2.txt", "r") as f:
+    lines = lines + [line.strip() for line in f]
+
+f.close()
+
+
+with open("data_reduced3.txt", "r") as f:
+    lines = lines + [line.strip() for line in f]
+
+f.close()
+
+
+
+program_starts = time.time()
+for line in lines:
+    new_dict = dict()
+    for field, value in zip(field_names, line.split("|")):
+        if value == "":
+            print("null spotted")
+            value = None
+
+        if value == "TRUE":
+            value = True
+        if value == "FALSE":
+            value = False
+        new_dict[field] = value
+
+    objs.append(Car(**new_dict))
+    now = time.time()
+Car.objects.bulk_create(objs)
+
