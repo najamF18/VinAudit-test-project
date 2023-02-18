290ee43ec21a31a8aa3b5f4e9f01f9e070f1d7c5
najam <najamazam420@gmail.com>
2023-02-18 11:00:25 -0600

    queries written to set price and mileage as zero if they are null

--- avg_price_calculator/utils.py
+++ avg_price_calculator/utils.py
@@ -6,6 +6,8 @@ def calculate_avg_price(input_mileage):
     """
     This function takes the filtered car objects and returns the calculated average price
     """
+    Car.objects.filter(listing_mileage=None).update(listing_mileage=0)
+    Car.objects.filter(listing_price=None).update(listing_price=0)
     all_cars = Car.objects.all()
     prices = list(all_cars.values_list("listing_price", flat=True))
     mileage = list(all_cars.values_list("listing_mileage", flat=True))
